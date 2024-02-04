136a08c119811020311f15a696e8259322fb2103
Prashanth Puranik <puranik@digdeeper.in>
2024-02-04 15:30:15 +0530

    Update sessions.ts

--- src-plain-react/services/sessions.ts
+++ src-plain-react/services/sessions.ts
@@ -4,17 +4,17 @@ import ISession from '../models/ISession';
 export type VoteType = 'upvote' | 'downvote'
 
 const getSessionsForWorkshop = async ( id : number | string ) => {
-    const response = await axios.get( `https://workshops-server.herokuapp.com/workshops/${id}/sessions` );
+    const response = await axios.get( `https://workshops-server.onrender.com/workshops/${id}/sessions` );
     return response.data;
 };
 
 const voteForSession = async ( id : number | string, voteType : VoteType ) => {
-    const response = await axios.put( `https://workshops-server.herokuapp.com/sessions/${id}/${voteType}` );
+    const response = await axios.put( `https://workshops-server.onrender.com/sessions/${id}/${voteType}` );
     return response.data;
 };
 
 const addSession = async ( session : Omit<ISession, 'id'> ) => {
-    const response = await axios.post( `https://workshops-server.herokuapp.com/sessions`, session, {
+    const response = await axios.post( `https://workshops-server.onrender.com/sessions`, session, {
         headers: {
             'Content-Type': 'application/json'
         }
